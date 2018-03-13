f4151c990c4c339ca94232a59f79dc4c2a7b66b2
Ruins He <lyhypacm@gmail.com>
2018-03-13 14:03:44 +0800

    Fix test.

--- test.py
+++ test.py
@@ -15,15 +15,17 @@ def RunTest(test_case_file):
             wrapped = line.strip()
             if not wrapped:
                 continue
-            language_token, source_file, time, memory, data, expected = wrapped.split(" ")
+            language_token, source_file, time, memory, data, expected = wrapped.split(
+                " ")
             actual = runner.Judge(
-                work_dir = work_dir, \
-                data_dir = work_dir, \
-                language_token = language_token, \
-                source_file = source_file, 
-                time_limit = int(time), \
-                memory_limit = int(memory), \
-                test_case = data)
+                work_dir=work_dir,
+                data_dir=work_dir,
+                language_token=language_token,
+                source_file=source_file,
+                time_limit=int(time),
+                memory_limit=int(memory),
+                test_case=data,
+                compile=True)
             assert actual.startswith(expected), \
                 "test case'%s': expected answer is %s but actual answer is %s." \
                 % (wrapped, expected, actual)
@@ -36,6 +38,7 @@ def RunTest(test_case_file):
         print "testing " + test_case_file + " \033[0;32mPASSED\033[m"
     else:
         print "testing " + test_case_file + " \033[0;31mFAILED\033[m"
+
 
 RunTest("test_case0")
 RunTest("test_case1")
