88b96f50893644c791544869080723d26deec4c4
LaPluses <xipersup@gmail.com>
2018-03-15 12:08:29 +0800

    Added socket to blacklist

--- tests/test_case20/Main.py
+++ tests/test_case20/Main.py
@@ -1,3 +1,22 @@
+import socket  
+phone = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+phone.bind(('172.17.21.56', 8080))  
+  
+phone.listen(5)  
+  
+print('starting....')  
+conn, addr = phone.accept()  
+print(conn)  
+print('client addr', addr)  
+print('ready to read msg')  
+client_msg = conn.recv(1024)  
+print('client msg: %s' % client_msg)  
+conn.send(client_msg.upper())  
+  
+conn.close()  
+phone.close() 
+
+'''
 import threading
 
 # 创建全局ThreadLocal对象:
@@ -19,24 +38,4 @@ t1.start()
 t2.start()
 t1.join()
 t2.join()
-
-
-'''
-import socket  
-phone = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-phone.bind(('172.17.21.56', 8080))  
-  
-phone.listen(5)  
-  
-print('starting....')  
-conn, addr = phone.accept()  
-print(conn)  
-print('client addr', addr)  
-print('ready to read msg')  
-client_msg = conn.recv(1024)  
-print('client msg: %s' % client_msg)  
-conn.send(client_msg.upper())  
-  
-conn.close()  
-phone.close() 
 '''
--- util.py
+++ util.py
@@ -6,20 +6,21 @@ from os import path
 c_language_blacklist = "!open:k,execve:k,flock:k,ptrace:k,sync:k,fdatasync:k,fsync:k,msync:k," + \
     "sync_file_range:k,syncfs:k,unshare:k,setns:k,clone:k,query_module:k,sysinfo:k,syslog:k,sysfs:k," + \
     "set_thread_area:k,mprotect:k,munmap:k,fork:k,vfork:k,getegid32:k,geteuid32:k,getuid32:k,getrlimit:k," + \
-    "openat:k,set_robust_list:k,sigprocmask:k,rt_sigprocmask:k,ugetrlimit:k,writev:k,close:k"
+    "openat:k,set_robust_list:k,sigprocmask:k,rt_sigprocmask:k,ugetrlimit:k,writev:k,close:k,socket:k"
 
 java_language_blacklist = "!ugetrlimit,uname,rt_sigprocmask,sigprocmask," + \
                           "set_robust_list,openat," + \
                           "getrlimit,getuid32,getgid32,geteuid32,getegid32,set_tid_address," + \
                           "flock,ptrace,sync,fdatasync,fsync,msync," + \
                           "sync_file_range,syncfs:k,unshare,setns,clone[a&268435456==268435456]," + \
-                          "query_module,sysinfo,syslog,sysfs,write,fork,vfork,munmap,set_thread_area"
+                          "query_module,sysinfo,syslog,sysfs,write,fork,vfork,munmap,set_thread_area,socket"
 
 python_language_blacklist = "!execve:k,flock:k,ptrace:k,sync:k,fdatasync:k,fsync:k,msync:k," + \
                             "sync_file_range:k,syncfs:k,unshare:k,setns:k,clone:k,query_module:k,syslog:k,sysfs:k," + \
                             "set_thread_area:k,fork:k,vfork:k,getegid32:k,geteuid32:k,getuid32:k," + \
                             "openat:k,sigprocmask:k,ugetrlimit:k,writev:k,sysinfo,getrlimit,munmap," + \
-                            "set_robust_list,rt_sigprocmask,ugetrlimit,getgid32,set_tid_address,clone[a&268435456==268435456]"
+                            "set_robust_list,rt_sigprocmask,ugetrlimit,getgid32,set_tid_address,clone[a&268435456==268435456]," + \
+                            "socket"
 
 
 judge_languages = {
